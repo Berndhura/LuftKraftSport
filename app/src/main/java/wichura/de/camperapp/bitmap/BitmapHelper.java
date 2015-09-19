f50995ce19946a2294a47cd4cd960df99e7f4a8a
Bernd Wichura <wichura@gmx.de>
2015-09-19 13:45:05 +0200

    add Picasso for bitmap waork

--- app/src/main/java/wichura/de/camperapp/bitmap/BitmapHelper.java
+++ app/src/main/java/wichura/de/camperapp/bitmap/BitmapHelper.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.util.Log;
 
+import com.squareup.picasso.Picasso;
+
 import java.io.IOException;
 
 /**
@@ -36,6 +38,13 @@ public class BitmapHelper {
     }
 
     public Bitmap resize(String uri){
+
+        /*Picasso.with(this.context)
+                .load(uri)
+                .resize(100,100)
+                .into();
+*/
+
         Bitmap bitmap = null;
         try {
             bitmap = MediaStore.Images.Media.getBitmap(
@@ -44,31 +53,13 @@ public class BitmapHelper {
             Log.i("MyActivity", "MyClass.getView() URLS " + "BITMAP FEHLER");
             e.printStackTrace();
         }
-//get the original size
+
         int orignalHeight = bitmap.getHeight();
         int orignalWidth = bitmap.getWidth();
 
         Bitmap thump = Bitmap.createScaledBitmap(bitmap,(int)(orignalWidth*0.1), (int)(orignalHeight*0.1), true);
 
-//initialization of the scale
-       // int resizeScale = 1;
-//get the good scale
-        //if ( orignalWidth > maxWidth || orignalHeight > maxHeight ) {
-        //    final int heightRatio = Math.round((float) orignalHeight / (float) maxHeight);
-        //    final int widthRatio = Math.round((float) orignalWidth / (float) maxWidth);
-        //    resizeScale = heightRatio < widthRatio ? heightRatio : widthRatio;
-       // }
-//put the scale instruction (1 -> scale to (1/1); 8-> scale to 1/8)
-        //opts.inSampleSize = resizeScale;
-        //opts.inJustDecodeBounds = false;
-//get the futur size of the bitmap
-        //int bmSize = (orignalWidth / resizeScale) * (orignalHeight / resizeScale) * 4;
-//check if it's possible to store into the vm java the picture
-        //if ( Runtime.getRuntime().freeMemory() > bmSize ) {
-//decode the file
-          //  bp = BitmapFactory.decodeFile(path, opts);
-        //} else
-         //   return null;
+
         return thump;
     }
 }
