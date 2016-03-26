16b2bce09cbca37e8b0910b97346b8f4ece6a51e
Bernd Wichura <wichura@gmx.de>
2016-03-26 22:22:40 +0100

    new design for OpenAdActivity, prepare google maps for location

--- app/src/main/java/wichura/de/camperapp/ad/OpenAdActivity.java
+++ app/src/main/java/wichura/de/camperapp/ad/OpenAdActivity.java
@@ -3,7 +3,6 @@ package wichura.de.camperapp.ad;
 import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
@@ -16,20 +15,33 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.ClientProtocolException;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
 import wichura.de.camperapp.R;
-import wichura.de.camperapp.app.AppController;
 import wichura.de.camperapp.http.Urls;
 
 public class OpenAdActivity extends Activity {
 
+    private static double longitute;
+    private static double latitude;
     private String pictureUri;
     private TextView mTitleText;
+    private TextView mPrice;
     private TextView mDescText;
     private TextView mLocationText;
     private TextView mPhoneText;
@@ -38,12 +50,8 @@ public class OpenAdActivity extends Activity {
 
     private ImageView imgView;
 
-    //TODO
-    //use swipe function from CustomSwipeAdapter to swipe pictures
-    private CustomSwipeAdapter adapter;
-    private ImageView viewPager;
-
-    ImageLoader imageLoader = AppController.getInstance().getImageLoader();
+    private int displayHeight;
+    private int displayWidth;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -52,47 +60,35 @@ public class OpenAdActivity extends Activity {
 
         setContentView(R.layout.open_ad_activity);
 
-        //TODO
-        //swipe the pictures, preparation
-        adapter = new CustomSwipeAdapter(this);
-        //error! this has to be a viewpager obj in layout xml file! TODO!
-        viewPager = (ImageView) findViewById(R.id.icon);
-        //viewPager.setAdapter(adapter);
+        getDisplaydimension();
 
-
-        TextView titelHeader = (TextView) findViewById(R.id.headerTitel);
         mTitleText = (TextView) findViewById(R.id.title);
-        TextView desHeader = (TextView) findViewById(R.id.headerDesciption);
+        mPrice = (TextView) findViewById(R.id.price);
         mDescText = (TextView) findViewById(R.id.description);
-        mLocationText = (TextView) findViewById(R.id.location);
-        mPhoneText = (TextView) findViewById(R.id.phone);
-        imgView = (ImageView) findViewById(R.id.icon);
+        // mLocationText = (TextView) findViewById(R.id.location);
+        //mPhoneText = (TextView) findViewById(R.id.phone);
+        imgView = (ImageView) findViewById(R.id.imageView);
         mDelButton = (Button) findViewById(R.id.delButton);
 
 
         //get data from Intent
         pictureUri = getIntent().getStringExtra("uri");
 
-        mTitleText.setText(getIntent().getStringExtra("title"));
-        mDescText.setText(getIntent().getStringExtra("description"));
-        mLocationText.setText(getIntent().getStringExtra("location"));
-        mPhoneText.setText(getIntent().getStringExtra("phone"));
+        mTitleText.setText("Titel der Anzeige");//(getIntent().getStringExtra("title"));
+        mPrice.setText("102€");//(getIntent().getStringExtra("title"));
+        mDescText.setText("Die einfachste Möglichkeit, Ihre App in Aktion zu sehen, besteht darin, ein Android-Gerät " +
+                "an Ihren Computer anzuschließen. Befolgen Sie die Anweisungen, um Entwickleroptionen in Ihrem Android-Gerät zu" +
+                " aktivieren und Ihre Anwendung und Ihr System so zu konfigurieren, dass das Gerät erkannt wird.");//(getIntent().getStringExtra("description"));
+        // mLocationText.setText(getIntent().getStringExtra("location"));
+        //mPhoneText.setText(getIntent().getStringExtra("phone"));
         mAdId = getIntent().getStringExtra("id");
 
-        //Displaygroesse
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        Log.i("CONAN", "X " +  size.x);
-        Log.i("CONAN", "Y " +  size.y);
+        int ratio = Math.round((float) displayWidth / (float) displayWidth);
 
-
-        ImageView picture = (ImageView) imgView
-                .findViewById(R.id.icon);
 
         Picasso.with(getApplicationContext())
                 .load(pictureUri)
-                .resize(100, 100)
+                .resize((int) Math.round((float) displayWidth * 0.6), (int) Math.round((float) displayHeight * 0.6) * ratio)
                 .centerCrop()
                 .into(imgView);
 
@@ -107,6 +103,27 @@ public class OpenAdActivity extends Activity {
         });
 
         Log.i("CONAN", "MyClass.getView() OPEN " + pictureUri);
+
+        //map fragment in app : https://developers.google.com/maps/documentation/android-api/start#die_xml-layoutdatei
+        //TODO: show location on map fragment
+        //TODO: get LatLng in JSON from  http://maps.google.com/maps/api/geocode/json?address=%22greifswald%22&sensor=false
+        //getLocationInfo()
+        //now get Lat and Lng  from  getLatLong()
+        // this:  http://stackoverflow.com/questions/3574644/how-can-i-find-the-latitude-and-longitude-from-address
+
+    }
+
+    private void getDisplaydimension() {
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        Log.i("CONAN", "X " + size.x);
+        Log.i("CONAN", "Y " + size.y);
+
+        displayWidth = size.x;
+        displayHeight = size.y;
+
     }
 
     private void deleteAdRequest(String adId) {
@@ -130,5 +147,57 @@ public class OpenAdActivity extends Activity {
         queue.add(stringRequest);
     }
 
+    public static JSONObject getLocationInfo(String address) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
 
+            address = address.replaceAll(" ", "%20");
+
+            HttpPost httppost = new HttpPost("http://maps.google.com/maps/api/geocode/json?address=" + address + "&sensor=false");
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response;
+            stringBuilder = new StringBuilder();
+
+
+            response = client.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            InputStream stream = entity.getContent();
+            int b;
+            while ((b = stream.read()) != -1) {
+                stringBuilder.append((char) b);
+            }
+        } catch (ClientProtocolException e) {
+        } catch (IOException e) {
+        }
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject = new JSONObject(stringBuilder.toString());
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return jsonObject;
+    }
+
+    public static boolean getLatLong(JSONObject jsonObject) {
+
+        try {
+
+            longitute = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lng");
+
+            latitude = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lat");
+
+        } catch (JSONException e) {
+            return false;
+
+        }
+
+        return true;
+    }
 }
