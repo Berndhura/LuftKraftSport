c63422660d28a69db730e37ce13a8893fc23bcc7
Bernd Wichura <wichura@gmx.de>
2016-05-11 12:59:24 +0200

    notification prepare for later

--- app/src/main/java/wichura/de/camperapp/mainactivity/StartActivity.java
+++ app/src/main/java/wichura/de/camperapp/mainactivity/StartActivity.java
@@ -1,10 +1,12 @@
 package wichura.de.camperapp.mainactivity;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,7 +14,6 @@ import wichura.de.camperapp.R;
 
 /**
  * Created by Bernd Wichura on 31.03.2016.
- *
  */
 public class StartActivity extends Activity {
 
@@ -41,9 +42,21 @@ public class StartActivity extends Activity {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                Intent intent = new Intent(StartActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(StartActivity.this)
+                                .setSmallIcon(R.drawable.applogo)
+                                .setContentTitle("My notification")
+                                .setContentText("Hello World!");
+
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                // notificationID allows you to update the notification later on.
+                mNotificationManager.notify(33 , mBuilder.build());
+
+
+//                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+//                startActivity(intent);
+//                finish();
             }
         });
         return startButton;
