c296825c53f7fc42f4133b938a64bb77c5334e9c
Bernd Wichura <wichura@gmx.de>
2016-10-25 21:29:49 +0200

    use FileUploadService -> MVP

---
+++ app/src/main/java/wichura/de/camperapp/ad/FileUploadService.java
@@ -0,0 +1,126 @@
+package wichura.de.camperapp.ad;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import cz.msebera.android.httpclient.Header;
+import wichura.de.camperapp.bitmap.BitmapHelper;
+import wichura.de.camperapp.http.Urls;
+import wichura.de.camperapp.mainactivity.Constants;
+import wichura.de.camperapp.models.MyAdsRowItem;
+
+/**
+ * Created by ich on 25.10.2016.
+ * Camper App
+ */
+
+public class FileUploadService {
+
+    private Context context;
+    private NewAdActivity view;
+
+    FileUploadService(Context context, NewAdActivity view) {
+        this.context = context;
+        this.view = view;
+
+    }
+
+    public void multiPost(Intent data) {
+
+        view.showProgress();
+
+        String title = data.getStringExtra(MyAdsRowItem.TITLE);
+        String description = data.getStringExtra(MyAdsRowItem.DESC);
+        String keywords = data.getStringExtra(MyAdsRowItem.KEYWORDS);
+        String picture = data.getStringExtra(MyAdsRowItem.FILENAME);
+        String price = data.getStringExtra(MyAdsRowItem.PRICE);
+        long date = data.getLongExtra(MyAdsRowItem.DATE, 0);
+
+        Uri fileUri = Uri.parse(picture.toString());
+        Log.i("CONAN", "fileURI: " + fileUri);
+
+        //thumbnail?
+        // Bitmap ThumbImage = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(imagePath), THUMBSIZE, THUMBSIZE);
+
+        String fileString = getRealPathFromUri(context, fileUri); //war bildCompresse
+        Log.i("CONAN", "fileString: " + fileString);
+
+
+        File file = new File(fileString.toString());
+        Log.i("CONAN", "file: " + file);
+        BitmapHelper bitmapHelper = new BitmapHelper(context);
+        final File reducedPicture = bitmapHelper.saveBitmapToFile(file);
+        Log.i("CONAN", "newFile: " + reducedPicture);
+
+
+        //todo: add more pics here??
+        RequestParams params = new RequestParams();
+        try {
+            params.put("image", reducedPicture);
+        } catch (FileNotFoundException e) {
+        }
+
+        params.put(Constants.TITLE, title);
+        params.put(Constants.DESCRIPTION, description);
+        params.put(Constants.KEYWORDS, keywords);
+        params.put("userid", getUserId());  //TODO: userid vs user_id in server!! server anpassen? -> jo eine stelle dort in saveNewAd
+        params.put(Constants.PRICE, price);
+        params.put(Constants.DATE, date);
+        Log.d("CONAN", "userid bei upload: " + getUserId());
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post(Urls.MAIN_SERVER_URL + Urls.UPLOAD_NEW_AD_URL, params, new FileAsyncHttpResponseHandler(reducedPicture) {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
+                Log.e("Volley Request Error", "no");
+                Toast.makeText(context, "Upload did not work!\n", Toast.LENGTH_LONG).show();
+                view.hideProgress();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, File file) {
+                Log.d("CONAN", "Picture uploaded");
+                Toast.makeText(context, "Upload...done!", Toast.LENGTH_SHORT).show();
+                //jetzt bilder neu laden!! finish() geht zu früh zurück in mainactivity
+                Boolean deletetd = reducedPicture.delete();
+                if (!deletetd)
+                    Toast.makeText(context, "Delete tempFile not possible", Toast.LENGTH_SHORT).show();
+                view.hideProgress();
+                view.finish();
+            }
+        });
+    }
+
+    public static String getRealPathFromUri(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    private String getUserId() {
+        SharedPreferences settings = context.getSharedPreferences("UserInfo", 0);
+        return settings.getString(Constants.USER_ID, "");
+    }
+}
--- app/src/main/java/wichura/de/camperapp/ad/NewAdActivity.java
+++ app/src/main/java/wichura/de/camperapp/ad/NewAdActivity.java
@@ -1,15 +1,10 @@
 package wichura.de.camperapp.ad;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -17,20 +12,8 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.FileAsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-
-import cz.msebera.android.httpclient.Header;
 import wichura.de.camperapp.R;
-import wichura.de.camperapp.bitmap.BitmapHelper;
-import wichura.de.camperapp.http.Urls;
-import wichura.de.camperapp.mainactivity.Constants;
 import wichura.de.camperapp.models.MyAdsRowItem;
 
 
@@ -46,15 +29,10 @@ public class NewAdActivity extends AppCompatActivity {
 
     private ImageView mImgOne;
     private ImageView mImgTwo;
-    private String title;
-    private String description;
-    private String keywords;
-    private String price;
-    private String picture;
-    private String userId;
-    private long date;
 
     private ProgressBar progress;
+
+    private FileUploadService fileUploadService;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -75,6 +53,8 @@ public class NewAdActivity extends AppCompatActivity {
             });
         }
 
+        fileUploadService = new FileUploadService(getApplicationContext(), this);
+
         progress = (ProgressBar) findViewById(R.id.upload_ProgressBar);
         progress.setVisibility(ProgressBar.GONE);
 
@@ -83,11 +63,6 @@ public class NewAdActivity extends AppCompatActivity {
         mKeywords = (EditText) findViewById(R.id.keywords);
         mImgOne = (ImageView) findViewById(R.id.imageButton);
         mPrice = (EditText) findViewById(R.id.preis);
-
-        SharedPreferences settings = getSharedPreferences("UserInfo", 0);
-        final String id = settings.getString(Constants.USER_ID, "");
-        userId = id;
-
 
         final Button submitButton = (Button) findViewById(R.id.uploadButton);
         submitButton.setOnClickListener(new OnClickListener() {
@@ -100,14 +75,21 @@ public class NewAdActivity extends AppCompatActivity {
                 final String keyWordsString = "zelt";
                 final long date = System.currentTimeMillis();
 
-                // Package ToDoItem data into an Intent
                 final Intent data = new Intent();
-                MyAdsRowItem.packageIntent(data, titleString, "apid", descString, keyWordsString, mImage,
-                        "TODO", "PHONE", price, date);
+                MyAdsRowItem.packageIntent(
+                        data,
+                        titleString,
+                        "apid",
+                        descString,
+                        keyWordsString,
+                        mImage,
+                        "TODO",
+                        "PHONE",
+                        price,
+                        date);
 
-                sendHttpToServer(data);
+                fileUploadService.multiPost(data);
                 setResult(RESULT_OK, data);
-                //old finnish TODO
             }
         });
 
@@ -116,7 +98,6 @@ public class NewAdActivity extends AppCompatActivity {
         getPictureButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(final View v) {
-
                 final Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
                 photoPickerIntent.setType("image/*");
                 startActivityForResult(photoPickerIntent, SELECT_PHOTO);
@@ -155,97 +136,13 @@ public class NewAdActivity extends AppCompatActivity {
         }
     }
 
-    private void sendHttpToServer(final Intent data) {
 
-        title = data.getStringExtra(MyAdsRowItem.TITLE);
-        description = data.getStringExtra(MyAdsRowItem.DESC);
-        keywords = data.getStringExtra(MyAdsRowItem.KEYWORDS);
-        picture = data.getStringExtra(MyAdsRowItem.FILENAME);
-        price = data.getStringExtra(MyAdsRowItem.PRICE);
-        date = data.getLongExtra(MyAdsRowItem.DATE, 0);
-
-        Log.d("query", title + description + keywords + picture);
-
-        multiPost();
-    }
-
-    public static String getRealPathFromUri(Context context, Uri contentUri) {
-        Cursor cursor = null;
-        try {
-            String[] proj = {MediaStore.Images.Media.DATA};
-            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-    }
-
-
-    //http://stackoverflow.com/questions/18288864/how-to-multipart-data-using-android-volley
-    public void multiPost() {
-
+    public void showProgress() {
         progress.setVisibility(ProgressBar.VISIBLE);
+    }
 
-        //findViewById(R.id.avloadingIndicatorView).setVisibility(View.VISIBLE);
-
-
-        Uri fileUri = Uri.parse(picture.toString());
-        Log.i("CONAN", "fileURI: " + fileUri);
-
-        //thumbnail?
-        // Bitmap ThumbImage = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(imagePath), THUMBSIZE, THUMBSIZE);
-
-        String fileString = getRealPathFromUri(getApplicationContext(), fileUri); //war bildCompresse
-        Log.i("CONAN", "fileString: " + fileString);
-
-
-        File file = new File(fileString.toString());
-        Log.i("CONAN", "file: " + file);
-        BitmapHelper bitmapHelper = new BitmapHelper(getApplicationContext());
-        final File reducedPicture = bitmapHelper.saveBitmapToFile(file);
-        Log.i("CONAN", "newFile: " + reducedPicture);
-
-
-        //todo: add more pics here??
-        RequestParams params = new RequestParams();
-        try {
-            params.put("image", reducedPicture);
-        } catch (FileNotFoundException e) {
-        }
-
-        params.put(Constants.TITLE, title);
-        params.put(Constants.DESCRIPTION, description);
-        params.put(Constants.KEYWORDS, keywords);
-        params.put("userid", userId);  //TODO: userid vs user_id in server!! server anpassen? -> jo eine stelle dort in saveNewAd
-        params.put(Constants.PRICE, price);
-        params.put(Constants.DATE, date);
-        Log.d("CONAN", "userid bei upload: " + userId);
-
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.post(Urls.MAIN_SERVER_URL + Urls.UPLOAD_NEW_AD_URL, params, new FileAsyncHttpResponseHandler(reducedPicture) {
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
-                Log.e("Volley Request Error", "no");
-                Toast.makeText(getApplicationContext(), "Upload did not work!\n", Toast.LENGTH_LONG).show();
-                progress.setVisibility(ProgressBar.GONE);
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, File file) {
-                Log.d("CONAN", "Picture uploaded");
-                Toast.makeText(getApplicationContext(), "Upload...done!", Toast.LENGTH_SHORT).show();
-                //jetzt bilder neu laden!! finish() geht zu früh zurück in mainactivity
-                Boolean deletetd = reducedPicture.delete();
-                if (!deletetd)
-                    Toast.makeText(getApplicationContext(), "Delete tempFile not possible", Toast.LENGTH_SHORT).show();
-                progress.setVisibility(ProgressBar.GONE);
-                finish();
-            }
-        });
+    public void hideProgress() {
+        progress.setVisibility(ProgressBar.GONE);
     }
 }
 
