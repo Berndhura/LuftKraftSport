9e73f1326fb2ba01ad1b49e2e33e0543add65cf9
Bernd Wichura <wichura@gmx.de>
2017-01-26 23:06:43 +0100

    tablayout in new ad

--- app/src/main/java/wichura/de/camperapp/activity/NewAdActivity.java
+++ app/src/main/java/wichura/de/camperapp/activity/NewAdActivity.java
@@ -208,19 +208,16 @@ public class NewAdActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Sell";
                 case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+                    return "Seek";
             }
             return null;
         }
