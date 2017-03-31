cd047066dcaa79619cc22ee234eb8055f7edda44
Bernd Wichura <wichura@gmx.de>
2017-03-31 22:13:12 +0200

    show city name from latlng in create Ad

--- app/src/main/java/de/wichura/lks/activity/NewAdActivity.java
+++ app/src/main/java/de/wichura/lks/activity/NewAdActivity.java
@@ -1,8 +1,16 @@
 package de.wichura.lks.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -12,30 +20,40 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.List;
 
 import de.wichura.lks.R;
 import de.wichura.lks.http.FileUploadService;
 import de.wichura.lks.http.Urls;
 import de.wichura.lks.mainactivity.Constants;
+import de.wichura.lks.presentation.NewArticlePresenter;
 
 import static de.wichura.lks.mainactivity.Constants.SHARED_PREFS_USER_INFO;
 
 
-public class NewAdActivity extends AppCompatActivity {
+public class NewAdActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private EditText mDescription;
     private EditText mTitle;
     private EditText mPrice;
 
 
-    private static final int SELECT_PHOTO = 100;
+    private static final int SELECT_PHOTO_ONE = 100;
+    private static final int SELECT_PHOTO_TWO = 101;
     private String mImage;
     private String mImage2;
-    private int pictureCount = 1;
+    private int pictureCount = 0;
 
     private ImageView mImgOne;
     private ImageView mImgTwo;
@@ -44,6 +62,7 @@ public class NewAdActivity extends AppCompatActivity {
 
     public ProgressBar progress;
 
+    private NewArticlePresenter presenter;
     private FileUploadService fileUploadService;
     private Button submitButton;
 
@@ -54,10 +73,23 @@ public class NewAdActivity extends AppCompatActivity {
     private LinearLayout emptyBackgroundLl;
     private LinearLayout mainLl;
 
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private TextView locationName;
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
         setContentView(R.layout.new_ad_acivity);
 
@@ -89,6 +121,7 @@ public class NewAdActivity extends AppCompatActivity {
         mainLl = (LinearLayout) findViewById(R.id.main_upload_linear_layout);
 
         fileUploadService = new FileUploadService(getApplicationContext(), this);
+        presenter = new NewArticlePresenter(getApplicationContext(), this);
 
         mDescription = (EditText) findViewById(R.id.new_ad_description);
         mTitle = (EditText) findViewById(R.id.new_ad_title);
@@ -98,30 +131,25 @@ public class NewAdActivity extends AppCompatActivity {
         hideProblem();
         mPrice = (EditText) findViewById(R.id.new_ad_price);
 
-        location = (ImageView) findViewById(R.id.create_change_location);
-        location.setOnClickListener(v -> {
-            Toast.makeText(this, "aufruf der location", Toast.LENGTH_SHORT).show();
-        });
-
         mImgOne.setOnClickListener((v) -> {
             final Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
             photoPickerIntent.setType("image/*");
             photoPickerIntent.putExtra("imageOne", true);
-            startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            startActivityForResult(photoPickerIntent, SELECT_PHOTO_ONE);
 
 
             // Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-            //startActivityForResult(cameraIntent, SELECT_PHOTO);
+            //startActivityForResult(cameraIntent, SELECT_PHOTO_ONE);
             //TODO camera plus photo picker
             //http://stackoverflow.com/questions/5991319/capture-image-from-camera-and-display-in-activity
         });
 
         mImgTwo.setOnClickListener(v -> {
-            /*final Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            final Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
             photoPickerIntent.setType("image/*");
             photoPickerIntent.putExtra("imageTwo", true);
-            startActivityForResult(photoPickerIntent, SELECT_PHOTO);*/
-            Toast.makeText(this, "Dies kommt bald... in Arbeit :-)", Toast.LENGTH_LONG).show();
+            startActivityForResult(photoPickerIntent, SELECT_PHOTO_TWO);
+            //Toast.makeText(this, "Dies kommt bald... in Arbeit :-)", Toast.LENGTH_LONG).show();
         });
 
 
@@ -133,8 +161,9 @@ public class NewAdActivity extends AppCompatActivity {
             mDescription.setText(getIntent().getStringExtra(Constants.DESCRIPTION));
             mPrice.setText(getIntent().getStringExtra(Constants.PRICE));
             articleIdForEdit = getIntent().getIntExtra(Constants.ARTICLE_ID, 0);
-            //TODO: show pic loading progress!!!
+
             String pictureUri = Urls.MAIN_SERVER_URL_V3 + "pictures/" + (getIntent().getStringExtra(Constants.AD_URL));
+            showProgress();
             Picasso.with(getApplicationContext())
                     .load(pictureUri)
                     .placeholder(R.drawable.empty_photo)
@@ -142,14 +171,13 @@ public class NewAdActivity extends AppCompatActivity {
                     .into(mImgOne, new Callback() {
                         @Override
                         public void onSuccess() {
-                           // mOpenAdProgressBar.setVisibility(ProgressBar.GONE);
+                            hideProgress();
                         }
 
                         @Override
                         public void onError() {
-                           // mOpenAdProgressBar.setVisibility(ProgressBar.GONE);
+                            hideProgress();
                             Toast.makeText(getApplicationContext(), "No network connection while loading picture!", Toast.LENGTH_SHORT).show();
-                           // showDefaultPic();
                         }
                     });
 
@@ -184,6 +212,33 @@ public class NewAdActivity extends AppCompatActivity {
                 fileUploadService.updateArticle(data);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private void setupLocation() {
+
+        locationName = (TextView) findViewById(R.id.create_location_name);
+        presenter.getCityNameFromLatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+
+        location = (ImageView) findViewById(R.id.create_change_location);
+        location.setOnClickListener(v -> {
+            //aufruf loco
+        });
+    }
+
+    public void setCityName(String city) {
+        locationName.setText(city);
     }
 
     public void showProblem(String error) {
@@ -228,13 +283,13 @@ public class NewAdActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
 
         switch (requestCode) {
-            case SELECT_PHOTO:
+            case SELECT_PHOTO_ONE:
                 if (resultCode == RESULT_OK && pictureCount < 4) {
                     isImageChanged = true;
                     final Uri selectedImage = imageReturnedIntent.getData();
                     mImage = selectedImage.toString();
                     switch (pictureCount) {
-                        case 1: {
+                        case 0: {
                             Picasso
                                     .with(getApplicationContext())
                                     .load(selectedImage)
@@ -244,7 +299,38 @@ public class NewAdActivity extends AppCompatActivity {
                             break;
                         }
                     }
+                    break;
                 }
+            case SELECT_PHOTO_TWO: {
+                final Uri selectedImage = imageReturnedIntent.getData();
+                mImage2 = selectedImage.toString();
+                Picasso
+                        .with(getApplicationContext())
+                        .load(selectedImage)
+                        .fit()
+                        .into(mImgTwo);
+                pictureCount++;
+                break;
+            }
+        }
+    }
+
+    public void getLatLngFromPlz(String zip) {
+        final Geocoder geocoder = new Geocoder(this);
+        try {
+            List<Address> addresses = geocoder.getFromLocationName(zip, 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                Address address = addresses.get(0);
+                // Use the address as needed
+                String message = String.format("Latitude: %f, Longitude: %f",
+                        address.getLatitude(), address.getLongitude());
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            } else {
+                // Display appropriate message when Geocoder services are not available
+                // Toast.makeToast(this, "Unable to geocode zipcode", Toast.LENGTH_LONG).show();
+            }
+        } catch (IOException e) {
+            // handle exception
         }
     }
 
@@ -280,6 +366,26 @@ public class NewAdActivity extends AppCompatActivity {
 
     public String getUserToken() {
         return getSharedPreferences(SHARED_PREFS_USER_INFO, 0).getString(Constants.USER_TOKEN, "");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        int accessCoarseLoc = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (accessCoarseLoc == PackageManager.PERMISSION_GRANTED)
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        //got the location -> get city name and show it on UI
+        setupLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
 
---
+++ app/src/main/java/de/wichura/lks/presentation/NewArticlePresenter.java
@@ -0,0 +1,61 @@
+package de.wichura.lks.presentation;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import de.wichura.lks.activity.NewAdActivity;
+import de.wichura.lks.http.GoogleService;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by ich on 31.03.2017.
+ * Luftkraftsport
+ */
+
+public class NewArticlePresenter {
+
+    private Context context;
+    private GoogleService googleService;
+    private NewAdActivity view;
+
+    public NewArticlePresenter(Context applicationContext, NewAdActivity view) {
+        this.context = applicationContext;
+        this.googleService = new GoogleService();
+        this.view = view;
+    }
+
+    public void getCityNameFromLatLng(Double lat, Double lng) {
+
+        Observable<JsonObject> getCityNameFromLatLng = googleService.getCityNameFrimLatLngObserv(lat, lng, false);
+
+        getCityNameFromLatLng
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<JsonObject>() {
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d("CONAN", "error in getting city name from google maps api: " + e.toString());
+                    }
+
+                    @Override
+                    public void onNext(JsonObject location) {
+                        JsonElement city = location.get("results").getAsJsonArray()
+                                .get(0).getAsJsonObject().get("address_components").getAsJsonArray()
+                                .get(2).getAsJsonObject().get("long_name");
+
+                        Log.d("CONAN", "city name from google maps api: " + city);
+                        view.setCityName(city.getAsString());
+                    }
+                });
+    }
+}
