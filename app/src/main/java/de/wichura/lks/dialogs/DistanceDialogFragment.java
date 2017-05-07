2860a2525ff35e4a0984b9ecc1bec479c5704514
Bernd Wichura <wichura@gmx.de>
2017-05-07 20:55:30 +0200

    distance dialog

--- app/src/main/java/de/wichura/lks/dialogs/DistanceDialogFragment.java
+++ app/src/main/java/de/wichura/lks/dialogs/DistanceDialogFragment.java
@@ -2,7 +2,6 @@ package de.wichura.lks.dialogs;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
@@ -40,60 +39,30 @@ public class DistanceDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-
-        /*
-         <LinearLayout
-        android:id="@+id/location_distance_view"
-        android:layout_width="match_parent"
-        android:layout_height="wrap_content"
-        android:layout_alignParentTop="true"
-        android:layout_margin="8dp"
-        android:background="#FFFFFF"
-        android:orientation="vertical">
-
-        <TextView
-            android:id="@+id/distance_header"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:padding="8dp"
-            android:text="Umkreis:" />
-
-        <SeekBar
-            android:id="@+id/distance_seek_for_dialog"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:padding="8dp" />
-         */
-
         LinearLayout view = new LinearLayout(getActivity());
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
         view.setLayoutParams(params);
         view.setOrientation(LinearLayout.VERTICAL);
 
-
         final SeekBar seek = new SeekBar(getActivity());
-        seek.setMax(500);
+        seek.setMax(100);
+        seek.setPadding(50, 20, 50, 20);
 
         TextView showDist = new TextView(getActivity());
+        showDist.setPadding(50, 50, 50, 20);
 
         seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progress = 0;
 
             public void onProgressChanged(SeekBar seekBar, int progressV, boolean fromUser) {
-                progress = progressV;
-                String v = seekBar.getProgress() + " km";
-                showDist.setText(v);
+                showDist.setText(getDistanceString(seekBar));
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {
-                String v = seekBar.getProgress() + " km";
-                showDist.setText(v);
-
+                showDist.setText(getDistanceString(seekBar));
             }
 
             public void onStopTrackingTouch(SeekBar seekBar) {
-                String v = seekBar.getProgress() + " km";
-                showDist.setText(v);
+                showDist.setText(getDistanceString(seekBar));
                 mListener.onDistanceComplete(seek.getProgress());
             }
         });
@@ -101,23 +70,17 @@ public class DistanceDialogFragment extends DialogFragment {
         view.addView(showDist);
         view.addView(seek);
 
-
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.distence_request)
                 .setView(view)
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        mListener.onDistanceComplete(seek.getProgress());
-                    }
-                })
-                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        getDialog().cancel();
-                    }
-                });
-
+                .setPositiveButton(android.R.string.yes, (dialog, which) ->
+                        mListener.onDistanceComplete(seek.getProgress()))
+                .setNegativeButton(android.R.string.no, (dialog, which) ->
+                        getDialog().cancel());
         return builder.create();
+    }
+
+    private String getDistanceString(SeekBar seekBar) {
+        return (seekBar.getProgress() == 100) ? "Überall" : seekBar.getProgress() * 5 + " km";
     }
 }
